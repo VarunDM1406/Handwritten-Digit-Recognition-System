63d1723079f2a9c7a39e9699852e33068cb06518
Varun Dev Mittal <varunm55@gmail.com>
2025-11-30 07:26:34 +0530

    Removed unwanted files from GitHub but kept them locally

--- main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-
-import keras 
-
-from keras.datasets import mnist
-(X_train, y_train), (X_test, y_test)=mnist.load_data()
-X_train.shape, y_train.shape, X_test.shape, y_test.shape
